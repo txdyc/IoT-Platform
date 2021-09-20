07be267831135d604cbd6dffd724a07cc420f761
Napster.Cao <txdyc@hotmail.com>
2021-09-20 17:41:40 +0800

    add application class

---
+++ message-handler/src/main/java/com/meslean/iotmessage/Application.java
@@ -0,0 +1,11 @@
+package com.meslean.iotmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
